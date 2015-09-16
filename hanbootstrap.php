24f69279aa0dc9177fb99fe04fa336d6460f2df9
Mike Louviere <mike.louviere@hansoninc.com>
2015-09-16 15:25:05 -0400

    Updating Version

--- hanbootstrap.php
+++ hanbootstrap.php
@@ -3,7 +3,7 @@
  * Plugin Name: HanBootStrapper for WordPress
  * Plugin URI: http://hansoninc.com
  * Description: This plugin works in conjunction with the internal HanBootStrapper JS. This plugin installs the hbs.js and allows developers to hook in controllers based on pages, sections and actions.
- * Version: 1.0.8.15
+ * Version: 1.1.9.16
  * Author: Mike Louviere / HansonInc
  * Author URI: http://hansoninc.com
  * License: GPL2
